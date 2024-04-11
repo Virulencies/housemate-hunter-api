7f1003189e96e406aab58691cb1a48e211a3c258
Erin Kelley <erin.kelley01@gmail.com>
2024-04-10 20:23:14 -0600

    Test

--- knexfile.js
+++ knexfile.js
@@ -1,75 +1,42 @@
-// // Update with your config settings.
+// Update with your config settings.
 
-// /**
-//  * @type { import("knex").Knex.Config }
-//  */
-// module.exports = {
-//   development: {
-//     client: 'pg',
-//     connection: 'postgres://localhost/roommates',
-//     migrations: {
-//       directory: './db/migrations'
-//     },
-//     seeds: {
-//       directory: './seeds'
-//     },
-//     useNullAsDefault: true
-//   },
-//   production: {
-//     client: 'pg',
-//     connection: process.env.DATABASE_URL,
-//     migrations: {
-//       directory: './db/migrations'
-//     },
-//     seeds: {
-//       directory: './seeds'
-//     },
-//     ssl: {
-//       rejectUnauthorized: false, 
-//     },
-//     useNullAsDefault: true
-//   }
-// };
-
-require('dotenv').config()
-
+/**
+ * @type { import("knex").Knex.Config }
+ */
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      url: process.env.DATABASE_URL,
-      charset: 'utf8',
-    },
-  },
-
-  staging: {
-    client: 'pg',
-    connection: {
-      url: process.env.DATABASE_URL,
-      charset: 'utf8',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    connection: 'postgres://localhost/roommates',
     migrations: {
-      tableName: 'knex_migrations',
+      directory: './db/migrations'
     },
+    seeds: {
+      directory: './seeds'
+    },
+    useNullAsDefault: true
   },
-
   production: {
     client: 'pg',
-    connection: {
-      url: process.env.DATABASE_URL,
-      charset: 'utf8',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations',
+      directory: './db/migrations'
     },
+    seeds: {
+      directory: './seeds'
+    },
+    ssl: {
+      rejectUnauthorized: false, 
+    },
+    useNullAsDefault: true
   },
+pool: {
+  min: 2,
+  max: 10,
+},
+migrations: {
+  tableName: 'knex_migrations',
 }
+};
+
+
   
