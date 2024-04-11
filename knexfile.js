188701b79e73cf6e0f406f24415c516cdd6fc9e6
Erin Kelley <erin.kelley01@gmail.com>
2024-04-10 20:15:56 -0600

    Config

--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,75 @@
-// Update with your config settings.
+// // Update with your config settings.
 
-/**
- * @type { import("knex").Knex.Config }
- */
+// /**
+//  * @type { import("knex").Knex.Config }
+//  */
+// module.exports = {
+//   development: {
+//     client: 'pg',
+//     connection: 'postgres://localhost/roommates',
+//     migrations: {
+//       directory: './db/migrations'
+//     },
+//     seeds: {
+//       directory: './seeds'
+//     },
+//     useNullAsDefault: true
+//   },
+//   production: {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL,
+//     migrations: {
+//       directory: './db/migrations'
+//     },
+//     seeds: {
+//       directory: './seeds'
+//     },
+//     ssl: {
+//       rejectUnauthorized: false, 
+//     },
+//     useNullAsDefault: true
+//   }
+// };
+
+require('dotenv').config()
+
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/roommates',
-    migrations: {
-      directory: './db/migrations'
+    connection: {
+      url: process.env.DATABASE_URL,
+      charset: 'utf8',
     },
-    seeds: {
-      directory: './seeds'
-    },
-    useNullAsDefault: true
   },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      url: process.env.DATABASE_URL,
+      charset: 'utf8',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      url: process.env.DATABASE_URL,
+      charset: 'utf8',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
-      directory: './db/migrations'
+      tableName: 'knex_migrations',
     },
-    seeds: {
-      directory: './seeds'
-    },
-    ssl: {
-      rejectUnauthorized: false, 
-    },
-    useNullAsDefault: true
-  }
-};
-
-
+  },
+}
   
