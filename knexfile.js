3bf550300263f8e5c634fc361bc54a7b24b7d82f
Erin Kelley <erin.kelley01@gmail.com>
2024-04-11 11:08:58 -0600

    test

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,36 @@
+// // Update with your config settings.
+
+// /**
+//  * @type { import("knex").Knex.Config }
+//  */
+// module.exports = {
+//   development: {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL ||'postgres://localhost/roommates',
+//     migrations: {
+//       directory: './db/migrations'
+//     },
+//     seeds: {
+//       directory: './seeds'
+//     },
+//     useNullAsDefault: true
+//   },
+//   production: {
+//     client: 'pg',
+//     connection: 'postgres://yxnxvudzvblxzm:a7af70e1dbb73ecdc727006bbe3d99df7cab08af88a72e1e909032c0fd61c94c@ec2-107-21-67-46.compute-1.amazonaws.com:5432/dbblj85nc3srbf',
+//     migrations: {
+//       directory: './db/migrations'
+//     },
+//     seeds: {
+//       directory: './seeds'
+//     },
+//     ssl: {
+//       rejectUnauthorized: false, 
+//     },
+//     useNullAsDefault: true
+//   }
+// };
+
 // Update with your config settings.
 
 /**
@@ -6,7 +39,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL ||'postgres://localhost/roommates',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/roommates',
     migrations: {
       directory: './db/migrations'
     },
@@ -17,7 +50,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://yxnxvudzvblxzm:a7af70e1dbb73ecdc727006bbe3d99df7cab08af88a72e1e909032c0fd61c94c@ec2-107-21-67-46.compute-1.amazonaws.com:5432/dbblj85nc3srbf',
+    connection: process.env.DATABASE_URL || 'postgres://yxnxvudzvblxzm:a7af70e1dbb73ecdc727006bbe3d99df7cab08af88a72e1e909032c0fd61c94c@ec2-107-21-67-46.compute-1.amazonaws.com:5432/dbblj85nc3srbf',
     migrations: {
       directory: './db/migrations'
     },
@@ -25,9 +58,8 @@ module.exports = {
       directory: './seeds'
     },
     ssl: {
-      rejectUnauthorized: false, 
+      rejectUnauthorized: false
     },
     useNullAsDefault: true
   }
 };
-
