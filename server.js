bb5b374a89a8b0012c6f6c4e60d2a8946a69c35b
Erin Kelley <erin.kelley01@gmail.com>
2024-04-12 13:18:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 const express = require('express');
 const app = express();
 const cors = require('cors');
